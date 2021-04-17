066745e6e578a4241e0769e42369cd424ddf4bbc
Manu <manpreet@randoms.io>
2021-04-17 14:26:17 +0530

    Initial Commit

---
+++ FigosTests/FigosTests.swift
@@ -0,0 +1,33 @@
+//
+//  FigosTests.swift
+//  FigosTests
+//
+//  Created by Manu on 17/04/21.
+//
+
+import XCTest
+@testable import Figos
+
+class FigosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
